3a528d7f2a717a8393d25a242e3b779b6c5ef7fe
rameshr85022-ops <rameshr85022@gmail.com>
2026-09-25 16:30:29 +0530

    Fix output message for non-vowel characters
    Corrected print statement for non-vowel case.

--- vowel.py
+++ vowel.py
@@ -2,5 +2,7 @@ ch = input("Enter a character: ")
 
 if ch in "aeiouAEIOU":
     print("It is a vowel")
+    print("it is not a vowel")
 else:
     print("It is a consonant")
+    print("it is not constant")
